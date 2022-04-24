4b84064cdf6beea2858abd9c813fdbe2c1d2e55f
MadalynM <mou21015@byui.edu>
2022-04-23 23:39:24 -0600

    Completed portal HTML

---
+++ js/lastupdated.js
@@ -0,0 +1,12 @@
+//let daynames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+//let d = new Date();
+//let dayName = daynames[d.getDay()];
+//let monthName = months[d.getMonth()];
+//let year = d.getFullYear();
+//let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+let lastModify = new Date (document.lastModified)
+
+document.getElementById("lastupdated").textContent = lastModify;
