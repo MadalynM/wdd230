d86bb10c1e3c7b2a168a7c0984c2c27c641af524
MadalynM <mou21015@byui.edu>
2022-05-23 00:33:12 -0600

    Chamber Meeting Message Added

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -21,3 +21,11 @@ dateField.innerHTML = `<em>${fullDate}</em>`;
 let lastModify = new Date (document.lastModified)
 
 document.getElementById("lastUpdated").textContent = lastModify;
+
+//Chamber Meeting Message
+document.querySelector("#chamberMeeting").style.display = "none";
+let day = new Date();
+let weekDay = day.getDay();
+if (weekDay == 1 || weekDay == 2) {
+    document.querySelector("#chamberMeeting").style.display = "block";
+}
