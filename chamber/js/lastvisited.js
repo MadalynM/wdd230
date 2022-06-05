31c331d1fc5ba8dfe355e828fa880745a3ddc47d
MadalynM <mou21015@byui.edu>
2022-06-05 06:03:19 -0600

    Discover Page Complete

---
+++ chamber/js/lastvisited.js
@@ -0,0 +1,12 @@
+const daysBetweenVisit = document.querySelector("#visits");
+
+let lastVisit = Number(window.localStorage.getItem("visit"));
+let today = Date.now();
+
+let daysMs = today - lastVisit;
+let days = daysMs / 86400000;
+let daysRounded = Math.round(days);
+
+daysBetweenVisit.textContent = daysRounded;
+
+localStorage.setItem("visit", today);
