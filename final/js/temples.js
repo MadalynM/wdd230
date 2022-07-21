7ffb2eab3dde985149d67a5f2abe204ac64a0aea
MadalynM <mou21015@byui.edu>
2022-07-21 08:41:59 -0600

    Code Cleaned Up

--- final/js/temples.js
+++ final/js/temples.js
@@ -33,8 +33,6 @@ function displayTemples(data) {
         let tempSessions = document.createElement('p');
         let closedTitle = document.createElement('h3');
         let tempClosed = document.createElement('ul');
-        let likeBtn = document.createElement('img');
-        let likedBtn = document.createElement('img');
 
         h2.textContent = `${temple.name}`;
         tempAddress.textContent = `${temple.address}`;
@@ -76,21 +74,6 @@ function displayTemples(data) {
             tempClosed.appendChild(listItem);
         }
 
-        //likeBtn.innerHTML = `<img class="show" src=images/like-black.png alt="Black like button">`;
-        //likeBtn.setAttribute('type', `image`);
-        //likeBtn.setAttribute('name', `like`);
-        likeBtn.setAttribute('src', `images/like-black.png`);
-        likeBtn.setAttribute('alt', `Black Like Button`);
-        likeBtn.setAttribute('id', `likeButton`);
-        likeBtn.classList.add('show');
-        //likedBtn.innerHTML = `<img class="hide" src=images/like-blue.png alt="Blue liked button">`;
-        //likedBtn.setAttribute('type', `image`);
-        //likedBtn.setAttribute('name', `like`);
-        likedBtn.setAttribute('src', `images/like-blue.png`);
-        likedBtn.setAttribute('alt', `Blue Liked Button`);
-        likedBtn.setAttribute('id', `likedButton`);
-        likedBtn.classList.add('hide');
-
         tempImage.setAttribute('src', temple.img);
         tempImage.setAttribute('alt', `Image of ${temple.name}`);
         tempImage.setAttribute('loading', 'lazy');
@@ -112,8 +95,6 @@ function displayTemples(data) {
         card.appendChild(tempSessions);
         card.appendChild(closedTitle);
         card.appendChild(tempClosed);
-        card.appendChild(likeBtn);
-        card.appendChild(likedBtn);
         
         document.querySelector('div.cards').appendChild(card);
 
